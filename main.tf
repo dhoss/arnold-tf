75580aff6c761e67058e063b7c3f72a1fc0540f0
Devin Austin <devin.austin@gmail.com>
2019-06-15 14:40:41 -0600

    added stages and pipeline with respective db environments

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+# TODO: configuration variables per environment
+# e.g.: dev instance should connect as arnold-dev to get-pumped-bot-test server, 
+# prod should connect as arnold to get-pumped server
+# database credentials as well
+
 terraform {
   backend "pg" {
   }
@@ -11,19 +16,44 @@ variable "app_name" {
   description = "Name of the Heroku app"
 }
 
-resource "heroku_app" "arnold" {
-  name = "${var.app_name}"
+resource "heroku_app" "arnold-stage" {
+  name = "${var.app_name}-stage"
   region = "us"
   buildpacks = [
       "heroku/java"
   ]
 }
 
-resource "heroku_addon" "database" {
-  app  = "${heroku_app.arnold.name}"
+resource "heroku_app" "arnold-prod" {
+  name = "${var.app_name}-prod"
+  region = "us"
+  buildpacks = [
+      "heroku/java"
+  ]
+}
+
+resource "heroku_pipeline" "arnold-app" {
+  name = "arnold-app"
+}
+
+resource "heroku_pipeline_coupling" "staging" {
+  app      = "${heroku_app.arnold-stage.name}"
+  pipeline = "${heroku_pipeline.arnold-app.id}"
+  stage    = "staging"
+}
+
+resource "heroku_pipeline_coupling" "production" {
+  app      = "${heroku_app.arnold-prod.name}"
+  pipeline = "${heroku_pipeline.arnold-app.id}"
+  stage    = "production"
+}
+
+resource "heroku_addon" "database-stage" {
+  app  = "${heroku_app.arnold-stage.name}-stage"
   plan = "heroku-postgresql:hobby-dev"
 }
 
-output "arnold_app_url" {
-  value = "https://${heroku_app.arnold.name}.herokuapp.com"
+resource "heroku_addon" "database-prod" {
+  app  = "${heroku_app.arnold-prod.name}-prod"
+  plan = "heroku-postgresql:hobby-dev"
 }
